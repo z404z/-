ca19457f25a3f5af43643c0a6461af30991d1325
z404z <deathlyhalloww@gmail.com>
2018-11-18 23:34:30 +0300

    1

--- WindowsFormsApp5/Form1.cs
+++ WindowsFormsApp5/Form1.cs
@@ -19,10 +19,15 @@ namespace WindowsFormsApp5
         private void button1_Click(object sender, EventArgs e)
         {
             FormArray formlist = new FormArray(textBox1.Text);
-            ClassQueue count = new ClassQueue.CreateArray();//
+            ClassQueue count = new ClassQueue.CreateArray();//0
             label1.Text = Convert.ToString(formlist.result);//почему конвертация не работает
             label2.Text = Convert.ToString(count.count);
             //обратиться к createarray методу класса1 с параметром textbox1.text
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
