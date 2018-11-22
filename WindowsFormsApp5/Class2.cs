85eb59ec5fdbceaa0fa9e7d14793e4f01383ede1
Bossvlll Lefar <deathlyhalloww@gmail.com>
2018-11-22 04:39:43 +0300

    22.11

--- WindowsFormsApp5/Class2.cs
+++ WindowsFormsApp5/Class2.cs
@@ -8,60 +8,44 @@ namespace WindowsFormsApp5
 {
     class FormArray
     {
-        public string result = "";
-        public int[] arrayOfNums;// могу ли я такое же поле создать для массива, который будет нужен в другом скрипте?
+        string result;
+        int[] arrayOfNums;// могу ли я такое же поле создать для массива, который будет нужен в другом скрипте?
         // как к нему обратиться из другого класса?
-
         public FormArray(string s)
-        {           
-            string entered_list = s;// принимаю строку 
-            string[] entered_ar = entered_list.Split();// создали буферный массив
-
-            int full = 0;//кол-во цифровых значений, нужно для создания массива чисел в будущем
-
-            for (int i = 0; i < entered_ar.Length; i++)//пока не кончится буфер...
-            {
-                if (entered_ar[i] != "")//...считаем кол-во того, что не пробел, а если...
-                {                       // ...это пробел, то пропускаем его...
-                    full++;             //...
-                }                       //
-            }            // нашли число элементов(не пустых строк)
-
-            int[] arrayOfNums = new int[full];// создали числовой массив длины, равной выясненному кол-ву элементов
-
-            int countTextArr = 0;// номер элемента текстового массива
-            int countNumArr = 0;// номер элемента числового массива
-            // перемещаем элементы текстового массива в числовой, проверяя на пустые символы
-            // для этого нам нужна длина текстового массива (entered_ar.Length) и позиция входящего в него элемента
-            while (countTextArr < entered_ar.Length)  //countTextArr должен быть меньше длины исходного массива на 1
-            {
-                if (entered_ar[countTextArr] != "")    // проверка на пустую строку 
-                {
-                    arrayOfNums[countNumArr] = Convert.ToInt16(entered_ar[countTextArr]); 
-                    // помещаем элемент текстового в числовой массив
-                    countTextArr += 1;  // указываем, откуда помещать далее
-                    countNumArr += 1;   // указываем, куда помещать далее
-                }
-                else countTextArr += 1;//если наткнулись на пробел, просто идем к следующему элементу текстового
-            }
-            // результатом всех манипуляций должен быть числовой массив arrayOfNums[]
-            // как в метод класса внести поля из конструктора.                       
-            for (int i = 0; i<full; i++)
-            {
-                result += arrayOfNums[i].ToString() + " ";
-            }           
+        {
+            result = s;    
         }
-        // результатом всех манипуляций должен быть числовой массив arrayOfNums[]
-        //public void print(int[] arrayOfNums, int full)// как в метод класса внести поля из конструктора.
-        //{  
+
+        public void MakeArr()
+        {
+            string[] sNum = result.Split();
+            int ful = 0;
+            // подсчёт чисел в набранной строке
+            for (int i = 0; i < sNum.Length; i++)
+                if (sNum[i] != "")
+                    ful++;
+
+            int dim = ful;
+            arrayOfNums = new int[ful];
+
+            //заполнение массива arr
+            int indxArr = 0;
+            for (int i = 0; i < sNum.Length; i++)
+                if (sNum[i] != "")
+                    arrayOfNums[indxArr++] = Convert.ToInt16(sNum[i]);
+        } //MakeArr
+        //public string print()
+        //{
+        //    string res = "";
+        //    MakeArr();
+
+        //    for (int i = 0; i < arrayOfNums.Length; i++)
+        //        res += Convert.ToString(arrayOfNums[i]) + " ";
+        //    return res;
         //}
     }
-}
-                             
-                   
-
-            
-                                             // установили размер численного массива, равный числу реальных элементов
+}       
+        // установили размер численного массива, равный числу реальных элементов
                                       //второй массив теперь состоит только из чисел
                                              // цикл формирует массив arrayOfNums[count_second_array] 
                                              // организуем вывод
